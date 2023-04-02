5569962f226ba0bcec748a127320da71666c8b2a
sfk345 <sonechka_27_01@mail.ru>
2023-04-02 19:29:58 +0700

    7

---
+++ views/site/addOffice.php
@@ -0,0 +1,7 @@
+<div class="fut-admissions">
+    <h2>Добавление кабинета</h2>
+    <div class="add-patient">
+        <label>Название кабинета <input type="text" name="ID_patient"></label>
+        <button>Добавить</button>
+    </div>
+</div>
